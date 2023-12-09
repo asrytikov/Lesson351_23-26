4c87d58b3824a8dfac98e95e9303451dbeea721d
asrytikov <asrytikov@gmail.com>
2023-12-09 20:05:42 +0400

    Initial commit

---
+++ src/main/java/com/example/lesson351/Lesson351Application.java
@@ -0,0 +1,13 @@
+package com.example.lesson351;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson351Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson351Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lesson351/Lesson351ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson351;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson351ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
